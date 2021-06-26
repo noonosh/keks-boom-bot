6d18d8d6eaac4ac7a4ae1fb6a103edbcb4fd7c87
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-06-26 15:28:06 +0500

    Add "How to use" video.

--- callbacks/how_to_use.py
+++ callbacks/how_to_use.py
@@ -1,3 +1,8 @@
 
 def instructions(update, context):
-    update.effective_message.reply_text('please Abdurahmon, provide text for this message')
+    chat_id = update.effective_chat.id
+    context.bot.send_video(chat_id,
+                           video="BAACAgIAAxkBAAIFBWDW_4KPRWFPBfdBvZvYN3tjaTnuAAK9DAACGVC4SlxU_LmBlQv8IAQ",
+                           caption="🔥 Спасибо, что заинтересовались продуктом <b>«KEKS-BOOM»</b>\n\n"
+                                   "📺 Просмотрите видео-инструкцию по использованию Кекс-Огней",
+                           parse_mode="HTML")
